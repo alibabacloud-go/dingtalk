84eba03a8c67707908b1bdf0037dc619d860e679
sdk-team <sdk-team@alibabacloud.com>
2021-03-01 07:21:50 +0000

    Online.

---
+++ hrbrain_1_0/client.go
@@ -0,0 +1,176 @@
+// This file is auto-generated, don't edit it. Thanks.
+/**
+ *
+ */
+package hrbrain_1_0
+
+import (
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	openapiutil "github.com/alibabacloud-go/openapi-util/service"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/alibabacloud-go/tea/tea"
+)
+
+type SyncDataHeaders struct {
+	CommonHeaders           map[string]*string `json:"commonHeaders,omitempty" xml:"commonHeaders,omitempty"`
+	XAcsDingtalkAccessToken *string            `json:"x-acs-dingtalk-access-token,omitempty" xml:"x-acs-dingtalk-access-token,omitempty"`
+}
+
+func (s SyncDataHeaders) String() string {
+	return tea.Prettify(s)
+}
+
+func (s SyncDataHeaders) GoString() string {
+	return s.String()
+}
+
+func (s *SyncDataHeaders) SetCommonHeaders(v map[string]*string) *SyncDataHeaders {
+	s.CommonHeaders = v
+	return s
+}
+
+func (s *SyncDataHeaders) SetXAcsDingtalkAccessToken(v string) *SyncDataHeaders {
+	s.XAcsDingtalkAccessToken = &v
+	return s
+}
+
+type SyncDataRequest struct {
+	ProjectId *string `json:"projectId,omitempty" xml:"projectId,omitempty"`
+	SchemaId  *string `json:"schemaId,omitempty" xml:"schemaId,omitempty"`
+	DataId    *string `json:"dataId,omitempty" xml:"dataId,omitempty"`
+	Content   *string `json:"content,omitempty" xml:"content,omitempty"`
+	EtlTime   *string `json:"etlTime,omitempty" xml:"etlTime,omitempty"`
+}
+
+func (s SyncDataRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s SyncDataRequest) GoString() string {
+	return s.String()
+}
+
+func (s *SyncDataRequest) SetProjectId(v string) *SyncDataRequest {
+	s.ProjectId = &v
+	return s
+}
+
+func (s *SyncDataRequest) SetSchemaId(v string) *SyncDataRequest {
+	s.SchemaId = &v
+	return s
+}
+
+func (s *SyncDataRequest) SetDataId(v string) *SyncDataRequest {
+	s.DataId = &v
+	return s
+}
+
+func (s *SyncDataRequest) SetContent(v string) *SyncDataRequest {
+	s.Content = &v
+	return s
+}
+
+func (s *SyncDataRequest) SetEtlTime(v string) *SyncDataRequest {
+	s.EtlTime = &v
+	return s
+}
+
+type SyncDataResponse struct {
+	Headers map[string]*string `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+}
+
+func (s SyncDataResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s SyncDataResponse) GoString() string {
+	return s.String()
+}
+
+func (s *SyncDataResponse) SetHeaders(v map[string]*string) *SyncDataResponse {
+	s.Headers = v
+	return s
+}
+
+type Client struct {
+	openapi.Client
+}
+
+func NewClient(config *openapi.Config) (*Client, error) {
+	client := new(Client)
+	err := client.Init(config)
+	return client, err
+}
+
+func (client *Client) Init(config *openapi.Config) (_err error) {
+	_err = client.Client.Init(config)
+	if _err != nil {
+		return _err
+	}
+	client.EndpointRule = tea.String("")
+	if tea.BoolValue(util.Empty(client.Endpoint)) {
+		client.Endpoint = tea.String("api.dingtalk.com")
+	}
+
+	return nil
+}
+
+func (client *Client) SyncData(request *SyncDataRequest) (_result *SyncDataResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := &SyncDataHeaders{}
+	_result = &SyncDataResponse{}
+	_body, _err := client.SyncDataWithOptions(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) SyncDataWithOptions(request *SyncDataRequest, headers *SyncDataHeaders, runtime *util.RuntimeOptions) (_result *SyncDataResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.ProjectId)) {
+		body["projectId"] = request.ProjectId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.SchemaId)) {
+		body["schemaId"] = request.SchemaId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.DataId)) {
+		body["dataId"] = request.DataId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Content)) {
+		body["content"] = request.Content
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.EtlTime)) {
+		body["etlTime"] = request.EtlTime
+	}
+
+	realHeaders := make(map[string]*string)
+	if !tea.BoolValue(util.IsUnset(headers.CommonHeaders)) {
+		realHeaders = headers.CommonHeaders
+	}
+
+	if !tea.BoolValue(util.IsUnset(headers.XAcsDingtalkAccessToken)) {
+		realHeaders["x-acs-dingtalk-access-token"] = headers.XAcsDingtalkAccessToken
+	}
+
+	req := &openapi.OpenApiRequest{
+		Headers: realHeaders,
+		Body:    openapiutil.ParseToMap(body),
+	}
+	_result = &SyncDataResponse{}
+	_body, _err := client.DoROARequest(tea.String("SyncData"), tea.String("hrbrain_1.0"), tea.String("HTTP"), tea.String("POST"), tea.String("AK"), tea.String("/v1.0/hrbrain/datas"), tea.String("none"), req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
